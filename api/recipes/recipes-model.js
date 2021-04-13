b6d56edc7233e77d1739fe2410c5206a6f0c60b5
Virginia Davenport <virginialdport@gmail.com>
2021-04-13 12:55:01 -0600

    adds recipe and steps in same post

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,15 +1,36 @@
 const db = require("../../data/db-config");
+const recipesRouter = require("./recipes-router");
 const findById = (id) => {
     return db('recipes')
         .where({id})
         .first()
+
+}
+
+const findRecipeById = async(id) => {
+    const recipe = await db('recipes').where({id}).first();
+    const steps = await db('steps').where({ recipe_id: id}).select('*');
+
+    const result = {...recipe, steps};
+    return result
 }
 const addRecipe = async(obj) => {
     const [id] = await db('recipes').insert(obj).returning('id')
-    return findById(id)
+    return findRecipeById(id)
+}
+
+// function addSteps(arr){
+//     return db('steps').insert(arr)
+// }
+
+const addSteps = async(arr) => {
+    const [id] = await db('steps').insert(arr).returning('recipe_id')
+    return findRecipeById(id)
 }
 
 module.exports = {
     findById,
-    addRecipe
+    addRecipe,
+    addSteps,
+    findRecipeById
 }
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -4,14 +4,32 @@ const Recipes = require("./recipes-model")
 
 //at the moment can handle the name only, no steps yet
 recipesRouter.post('/', (req, res) => {
+    //splits name from rest of body
     const name = {name:req.body.name}
     Recipes.addRecipe(name)
         .then(recipe => {
-            res.status(200).json({data: recipe})
+            //create array for steps once a recipe_id has been created
+            const stepsMap = req.body.steps.map((step) => ({
+                recipe_id: recipe.id,
+                order: step.order,
+                step: step.step
+            }))
+            Recipes.addSteps(stepsMap).then(() => {
+                res.status(201).json({message: "recipe and steps added", data: recipe})
+            });
+            // res.status(200).json({data: recipe})
         })
         .catch(err => {
             res.status(500).json({data:req.body, err: err})
         })
 
+})
+
+recipesRouter.get('/:id', (req,res) => {
+    const {id} = req.params;
+    Recipes.findRecipeById(id)
+        .then((evt) => {
+            res.status(200).json(evt)
+        })
 })
 module.exports = recipesRouter
